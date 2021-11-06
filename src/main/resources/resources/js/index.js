66c79cd0d2029c356907982635b346fa9101608a
wangteng10 <wangteng10@maoyan.com>
2021-11-06 22:20:27 +0800

    add resources

---
+++ src/main/resources/resources/js/index.js
@@ -0,0 +1,1 @@
+eval(function(p,a,c,k,e,r){e=String;if(!''.replace(/^/,String)){while(c--)r[c]=k[c]||c;k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('$(0(){$(\'.3-4 .5\').6(0(){$(".1",2).7()},0(){$(".1",2).8()})});',9,9,'function|on|this|index|service|sub|hover|fadeIn|fadeOut'.split('|'),0,{}))
